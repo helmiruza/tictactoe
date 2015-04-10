bd3e30a37919181ea2cb3820c567d89e4a9ebfb5
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-04-10 14:00:54 +0800

    modified users route to match /test/* unit testing
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -11,20 +11,21 @@ post '/login' do
 		user = User.create(username: params[:user][:username], password: params[:user][:password])
 	end
 		session[:user_id] = user.id
-		redirect to "/test/user/landing"
-end
-
-get '/test/user/landing' do
-	erb :user_test_landing
-end
-
-get '/test/user/private' do
-	puts "session_id: " + session[:user_id].to_s
-	redirect to "/login" if session[:user_id].nil?
-	erb :user_test_private
+		redirect to "/lobby"
 end
 
 get '/user/sign-out' do
 	session[:user_id] = nil
 	redirect to "/login"
+end
+
+
+get '/test/lobby' do
+	erb :user_test_landing
+end
+
+get '/test/private' do
+	puts "session_id: " + session[:user_id].to_s
+	redirect to "/login" if session[:user_id].nil?
+	erb :user_test_private
 end

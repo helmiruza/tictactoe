78dbe701c226f8af9bcbbf4a8389ed9d8e11de26
helmiruza <helmiruza@gmail.com>
2015-04-10 09:21:30 +0000

    amended jquery and ajax
    Signed-off-by: helmiruza <helmiruza@gmail.com>

--- public/js/application.js
+++ public/js/application.js
@@ -2,56 +2,51 @@ $(document).ready(function(n){
   
   // var winner = 0;
   var n = $("#gamecount").val();
-  
-  $(".col").click(function(){
 
-    console.log("n:" + n)
-      if(n % 2 == 0) {
+  if($("#gamewinner").val() != ""){
+    alert($("#gamewinner").val() + " has won!");
+    
 
-        $(this).text("O");
+  }
+  else {
+
+    $(".col").click(function(){
+
+        if(n % 2 == 0) {
+
+          $(this).text("O");
+          
+          $("#movevalue").val("O");
+          $("#elementid").val($(this).attr("id"));
+          $("#symbol").val("O");
+          $("#winnerplayerid").val("Player1");
+
+          $("#playermove").submit();
         
-        $("#movevalue").val("O");
-        $("#elementid").val($(this).attr("id"));
-        $("#playermove").submit();
-        // check if player 1 won
-        $("#symbol").val("O");
-        $("#winnerplayerid").val("Player1");
-        $("#winnercheck").submit();
+          
+        }
 
-        // $("#gamewinner").val()
-        console.log($("#gamewinner").val())
-        // if($("#gamewinner").val() != "" ) {
-        //   alert("$("#gamewinner").val()" + " has won!");
+        else if(n % 2 == 1) {
+          $(this).text("X");
+          
+          $("#movevalue").val("X");
+          $("#elementid").val($(this).attr("id"));
+          $("#playermove").submit();
+          // check if player 2 won
+          $("#symbol").val("X");
+          $("#winnerplayerid").val("Player2");
+          $("#winnercheck").submit();
+          
 
-        // }
-        
-      }
+        }
 
-      else if(n % 2 == 1) {
-        $(this).text("X");
-        
-        $("#movevalue").val("X");
-        $("#elementid").val($(this).attr("id"));
-        $("#playermove").submit();
-        // check if player 2 won
-        $("#symbol").val("X");
-        $("#winnerplayerid").val("Player2");
-        $("#winnercheck").submit();
-        
-        // if($("#gamewinner").val() != "" ) {
-        //   alert("$("#gamewinner").val()" + " has won!");
+        else if (count == 9) {
+        	alert("it's a draw!");
+        }
 
-        // }
-        
-
-      }
-
-      else if (count == 9) {
-      	alert("it's a draw!");
-      }
-
-  
-  });
-  
+    
+    });
+  }      
 
 });
+

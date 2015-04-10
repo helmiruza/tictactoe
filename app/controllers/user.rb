7ad33bc1d0691eb1ebddb2e843f9eebc630c8f80
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-04-10 10:15:13 +0800

    Integrated user login and sign-up into master
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -3,9 +3,15 @@ get '/login' do
 end
 
 post '/login' do
-	user = User.find_or_create_by(params[:user])
-	session[:user_id] = user.id
-	redirect to "/test/user/landing"
+	puts params
+	if params[:user][:login] == "true"
+		user = User.authenticate(params[:user])
+		redirect to "/login" if user.nil?
+	elsif params[:user][:sign_up] == "true"
+		user = User.create(username: params[:user][:username], password: params[:user][:password])
+	end
+		session[:user_id] = user.id
+		redirect to "/test/user/landing"
 end
 
 get '/test/user/landing' do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  def self.authenticate(input)
+  	user = User.find_by(username: input[:username])
+  	return nil if user.nil?
+  	return user if user.password == input[:password]
+  	nil
+  end
 end
